2070735f61a28e55ee7ef3d69e3485ae26aaec2b
dhaeufel <david_zacharie.haeufel@stud.htwk-leipzig.de>
2023-08-21 14:33:51 +0200

    main ordnung schaffen

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,12 @@
-import LeInScraper from "./LeInScraper";
-import RdfParser from "./RdfParser";
+import LeInUploader from "./LeInUploader";
 //import UploadService from "./UploadService";
 import ExtractSrassenverzeichnis from "./scraping_strassenverzeichnis";
 
 async function main(){
 
-    const leInScraper= new LeInScraper();
-    await leInScraper.initUrlHtml();
-    const sightsName = leInScraper.getData('name');
-    const sightsStreet = leInScraper.getData('streetAddress')
-    const sightsImage = leInScraper.getImageUrls();
-    const sightsHref = leInScraper.getHrefs();
-    const sightsStreetWithoutNumber: string[] = [];
-    sightsStreet.forEach(inputString => {
-        sightsStreetWithoutNumber.push(leInScraper.deleteHouseNumber(inputString));
-    });
-
+    const leInUploader = new LeInUploader();
+    leInUploader.uploadLeInData();
     
-    const rdfParser = new RdfParser();
-    const triples = rdfParser.createTriples(sightsHref, `http://schema.org/name`, sightsName)
-    .concat(rdfParser.createTriples(sightsHref, `http://schema.org/image`, sightsImage))
-    .concat(rdfParser.createTriples(sightsHref, `http://schema.org/PostalAddress`, sightsStreetWithoutNumber));
-
-    const rdfData = rdfParser.createRdfData(triples);
-
-    console.log(rdfData);
     const extractSrassenverzeichnis = new ExtractSrassenverzeichnis();
     const parsedXmlData = await extractSrassenverzeichnis.parseXmlFile();
     if (parsedXmlData) {
