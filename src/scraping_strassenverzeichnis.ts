a0e567d9d95a62c2891bc14d5a54528eefddd4a2
phublitz <paulhublitz@googlemail.com>
2023-08-21 14:57:51 +0200

    main aufgeräumt in DataUploader

--- src/scraping_strassenverzeichnis.ts
+++ src/scraping_strassenverzeichnis.ts
@@ -15,15 +15,13 @@ export default class ExtractSrassenverzeichnis {
             if (result && result.Strassenverzeichnis && result.Strassenverzeichnis.STRASSE && Array.isArray(result.Strassenverzeichnis.STRASSE)) {
                 const strassen = result.Strassenverzeichnis.STRASSE;
                 
-
-
                 for (const strasseData of strassen) {
                     const stammdaten = strasseData.STAMMDATEN && Array.isArray(strasseData.STAMMDATEN) ? strasseData.STAMMDATEN[0] : null;
                     const erlaeuterung = strasseData.ERKLAERUNG && Array.isArray(strasseData.ERKLAERUNG) ? strasseData.ERKLAERUNG[0].ERLAEUTERUNG[0] : null;
 
                     if (stammdaten) {
-                        const name = stammdaten.NAME && stammdaten.NAME[0].trim(); // Hier die trim()-Methode anwenden
-                        const schluessel = stammdaten.SCHLUESSEL && stammdaten.SCHLUESSEL[0].trim(); // Hier die trim()-Methode anwenden
+                        const name = stammdaten.NAME && stammdaten.NAME[0].trim(); 
+                        const schluessel = stammdaten.SCHLUESSEL && stammdaten.SCHLUESSEL[0].trim(); 
 
                         if (name && erlaeuterung && schluessel) {
                             const triple = new Triple(name , erlaeuterung, schluessel)
